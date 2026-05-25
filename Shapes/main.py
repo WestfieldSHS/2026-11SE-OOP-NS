3cd5fac752e0eb1f4b6f8817f5bc20fddd59b64c
nadaa <nada.souksavong@education.nsw.gov.au>
2026-05-25 23:53:09 +0000

    drawing rectangle, fixing  issues with code

--- Shapes/main.py
+++ Shapes/main.py
@@ -51,8 +51,39 @@ print(f'Perimeter: {Square3.get_perimeter()}')
 Square3.draw()
 print()
 
+class Rectangle:
+    def __init__(self, x, y, width, height, color):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.color = color
+
+    def get_area(self):
+        return self.width * self.height
+
+    def get_perimeter(self):
+        return 2 * (self.width + self.height)
+
+    def draw(self):
+        for i in range(self.height):
+            print(self.color + " " * self.width)
 
 
+Rectangle1 = Rectangle(0, 0, 5, 3, "red")
+print(f'Area: {Rectangle1.get_area()}')
+print(f'Perimeter: {Rectangle1.get_perimeter()}')
+Rectangle1.draw()
+print()
 
+Rectangle2 = Rectangle(0, 0, 10, 5, "blue")
+print(f'Area: {Rectangle2.get_area()}')
+print(f'Perimeter: {Rectangle2.get_perimeter()}')
+Rectangle2.draw()
+print()
 
-
+Rectangle3 = Rectangle(0, 0, 15, 10, "green")
+print(f'Area: {Rectangle3.get_area()}')
+print(f'Perimeter: {Rectangle3.get_perimeter()}')
+Rectangle3.draw()
+print()
