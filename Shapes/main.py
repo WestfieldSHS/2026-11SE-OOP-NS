131c7d74ffc34fd731b61690225a746fef723c07
nadaa <nada.souksavong@education.nsw.gov.au>
2026-05-25 23:33:31 +0000

    draw and get_perimeter methods added to square class

--- Shapes/main.py
+++ Shapes/main.py
@@ -1,11 +1,47 @@
+class Shape:
+    x = 0
+    y = 0 
+    colour = ""
 
-from Shapes import *
+    def get_area(self):
+        """Calculate the area of the shape and return the value"""
+        pass
 
-Square1 = Square(0,0,5,"red")
+    def get_perimeter(self):
+        """Calculate the perimeter of the shape and return the value"""
+        pass
+    
+    def draw(self):
+        """Draw the shape on the screen using the position, size and colour"""
+        pass    
+
+class Square:
+    def __init__(self, x, y, length, color):
+        self.x = x
+        self.y = y
+        self.length = length
+        self.color = color
+
+    def get_area(self):
+        return self.length * self.length
+
+    def get_perimeter(self):
+        return self.length * 4
+
+    def draw(self):
+        for i in range(self.length):
+            print(self.color + " " * self.length)
+
+Square1 = Square(0, 0, 5, "red")
 print(Square1.get_area())
+print(Square1.get_perimeter())
+Square1.draw()
 
-Square2 = Square(0,0,10,"blue")
+Square2 = Square(0, 0, 10, "blue")
 print(Square2.get_area())
+print(Square2.get_perimeter())
+
+
 
 
 
