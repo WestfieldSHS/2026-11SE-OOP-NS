13cad21b68b5b8da0ae7b1f01696cfd4fe8f607b
nadaa <nada.souksavong@education.nsw.gov.au>
2026-05-25 03:09:22 +0000

    Completing the lesson Mr Evans assigned

---
+++ Shapes/main.py
@@ -0,0 +1,10 @@
+
+from Shapes import *
+
+shape1 = Shape()
+shape1.x = 10
+shape1.y = 20
+shape1.colour = "red"
+
+print(shape1.colour)
+
---
+++ Shapes/shape.py
@@ -0,0 +1,39 @@
+class Shape:
+    x = 0
+    y = 0 
+    colour = ""
+
+    def get_area(self):
+        """Calculate the area of the shape and return the value"""
+        pass
+
+    def get_perimeter(self):
+        """Calculate the perimeter of the shape and return the value"""
+        pass
+    
+    def draw(self):
+        """Draw the shape on the screen using the position, size and colour"""
+        pass
+
+    class Square(Shape):
+        def __init__(self, x, y, length, colour):
+            self.x = x
+            self.y = y
+            self.length = length
+            self.colour = colour
+
+class Circle(Shape):
+    def __init__(self, x, y, radius, colour):
+        self.x = x
+        self.y = y
+        self.radius = radius
+        self.colour = colour
+
+class Rectangle(Shape):
+    def __init__(self, x, y, width, height, colour):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.colour = colour
+        
