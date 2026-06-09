0e8c8d5d885de5c2cefb8538f0c71e7a88be30d9
nadaa <nada.souksavong@education.nsw.gov.au>
2026-06-09 00:08:21 +0000

    COMPLETING ACTIVITY

--- book encapsulation/book.py
+++ book encapsulation/book.py
@@ -26,10 +26,18 @@ class Book:
 
 # Example
 if __name__ == "__main__":
-    book = Book("Sunrise on the Reaping", "Suzanne Collins", date(2025, 4, 18), 400)
-    print("Title:", book.get_title())
-    print("Author:", book.get_author())
-    print("Publication Date:", book.get_publication_date())
-    print("Number of Pages:", book.get_num_pages())
+    book1 = Book("Sunrise on the Reaping", "Suzanne Collins", date(2025, 4, 18), 400)
+    print("Title:", book1.get_title())
+    print("Author:", book1.get_author())
+    print("Publication Date:", book1.get_publication_date())
+    print("Number of Pages:", book1.get_num_pages())
+
+    # This will raise an error due to invalid number of pages
+    try:
+        book2 = Book("Invalid Book", "Unknown Author", date(2025, 1, 1), -50)
+    except ValueError as e:
+        print("Error:", e)
+
+    
 
        
