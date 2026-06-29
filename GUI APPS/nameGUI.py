8fd79596fb7419c7e9f17f92947f72a0c48c23a5
nadaa <nada.souksavong@education.nsw.gov.au>
2026-06-29 02:42:19 +0000

    Completing the third Tkinter app, nameGUI.py

---
+++ GUI APPS/nameGUI.py
@@ -0,0 +1,24 @@
+from tkinter import *
+from tkinter import messagebox
+
+def SayHello():
+    global txtFirstname, txtLastname
+
+    firstname = txtFirstname.get()
+    lastname = txtLastname.get()
+
+    messagebox.showinfo("Hello", f"Hello {firstname} {lastname}")
+
+    root = Tk()
+
+    Label(root, text="First name").grid(row=0)
+txtFirstname = Entry(root)
+txtFirstname.grid(row=0, column=1)
+
+Label(root, text="Last name").grid(row=1)
+txtLastname = Entry(root)
+txtLastname.grid(row=1, column=1)
+
+Button(root, text="Say Hello", command=SayHello).grid(row=2, columnspan=2)
+
+root.mainloop()
