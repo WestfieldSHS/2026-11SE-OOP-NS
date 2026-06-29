939b0de79dea519475ad7e0c71f7b6acd7dcc0bc
nadaa <nada.souksavong@education.nsw.gov.au>
2026-06-29 02:20:43 +0000

    Doing Mr Dingo's canva slides for the first activity in GUI

---
+++ GUI APPS/helloGUI.py
@@ -0,0 +1,11 @@
+from tkinter import *
+
+root = Tk()
+lbl = Label(root, text="Hello, World!")
+lbl.pack()
+root.mainloop()
+
+btn = Button(root, text="Goodbye", width=50, command=root.destroy)
+btn.pack()
+root.mainloop()
+
